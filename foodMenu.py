b927370ce6140cced379c2c4ad89b2dc2229f6d3
Vaibhavd1607 <deshmukhvaibhav078@gmail.com>
2022-08-24 23:08:44 +0530

    food menu features

---
+++ foodMenu.py
@@ -0,0 +1,35 @@
+print("1monday\n 2tue\n3wed\n4thurs")
+day=int(input("enter a day : "))
+if day==1:
+    b=int(input("enter a time:\n 1breakfast\n2lunch\n3dinner : "))
+if b==1:
+    print("1maggi\n2sandwich\n3samosa")
+    c=int(input("choose an optn :"))
+    if c==1:
+    	print("maggi of rs 150" )
+    elif c==2:
+    	 print("thanku for order")
+    elif c==3:
+    	print("thanku for order")
+    else:
+        print("invalid order")
+    	
+elif b==2:
+ print("1dal\n2chawl\n3razma")  
+d=int(input("choose an optn :"))
+if d==1:
+         print("thanku for order")
+elif d ==2:
+ 	    print("thanku for order")
+else:
+ 	    print("thanku for order")
+ 	    
+if b==3:
+	print("1gobi\n2aaloo\n3chhole")
+e=int(input("choose an optn :"))
+if e==1:
+ 	print("thanku for order")
+elif e==2:
+ 	print("thanku for order")
+else:
+ 	print('ty for ordr')
